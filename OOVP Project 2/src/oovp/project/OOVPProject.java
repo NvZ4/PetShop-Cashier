ecb10a36a09b306c97073a402bdc438dd19352fc
Haris Muhyidin Shofar <harismuhyidinshofar45@gmail.com>
2025-07-21 16:31:57 +0700

    Upload all Files

---
+++ OOVP Project 2/src/oovp/project/OOVPProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oovp.project;
+
+/**
+ *
+ * @author lenov
+ */
+public class OOVPProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
